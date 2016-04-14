eb16db110feed7bd775f043081b2f2c1ddd78c10
Adam Wick <awick@dunworthy.local>
2016-04-13 20:00:09 -0700

    Reorganization. I think I like this better?

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+buildImage () {
+  dockerFile=$1
+  imageName=$2
+  imageTag=$3
+
+  tmpdir=`mktemp -d -t halvmdocker-XXXXXX` || return 1
+  cp $1 ${tmpdir}/Dockerfile
+  docker build -t ${imageName}:${imageTag} ${tmpdir} || (rm -rf ${tmpdir} && return 2)
+  rm -rf ${tmpdir}
+  return 0
+}
+
+buildImageFromFile () {
+  file=$1
+  name=halvm/`echo ${file} | sed -e "s!/.*!!g"`
+  tag=`echo ${file} | sed -e "s/^.*Dockerfile.//g"`
+  echo "Got name '${name}' and tag '${tag}' from ${file}"
+  buildImage "${file}" "${name}" "${tag}"
+  return $?
+}
+
+buildImagesForSystem () {
+  dir=$1
+  images=`ls ${dir}*/Dockerfile.*`
+  for i in ${images}; do
+    buildImageFromFile ${i} || return 3
+  done
+}
+
+# buildImagesForSystem "base" || exit $?
+# buildImagesForSystem "extended"
+buildImageFromFile "extended-gmp/Dockerfile.fedora23"
